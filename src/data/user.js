131913cf8b9d5db26bb312ddd7f51173a8e206d6
iqra <abdikarimiqra@gmail.com>
2025-04-10 23:03:52 +0300

    index.html

--- src/data/user.js
+++ src/data/user.js
@@ -3,4 +3,18 @@ const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
+// function username (){
+//     console.log("Liza");
+// }
+
+// function city () {
+//     console.log("New York");
+// }
+
+// function image (){
+//     console.log("https://i.imgur.com/mV8PQxj.gif");
+// }
+
+export {username, city, image}
+
 // export all three variables as named exports
